155b665f3cd87eb4960ef0b9a67670ef9ee73a00
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-02 14:19:42 +0400

    Day 2: refactoring

--- Day-2-Cube-Conundrum/main.cpp
+++ Day-2-Cube-Conundrum/main.cpp
@@ -32,6 +32,16 @@ bool is_valid_step(const string& step) {
     return is_valid_step(red_count, green_count, blue_count);
 }
 
+bool is_valid_game(const string& game) {
+    stringstream game_ss(game);
+    string step;
+    bool valid = true;
+    while (getline(game_ss, step, ';')) {
+        valid = valid && is_valid_step(step);
+    }
+    return valid;
+}
+
 int main(int argc, char** argv) {
     ifstream ifs(argv[1]);
     string line;
@@ -39,13 +49,10 @@ int main(int argc, char** argv) {
     while (getline(ifs, line)) {
         stringstream line_ss(line);
         int id;
-        string skip, step;
+        string skip, game;
         line_ss >> skip >> id >> skip;
-        bool valid = true;
-        while (getline(line_ss, step, ';')) {
-            valid = valid && is_valid_step(step);
-        }
-        if (valid) {
+        getline(line_ss, game);
+        if (is_valid_game(game)) {
             ans += id;
         }
     }
