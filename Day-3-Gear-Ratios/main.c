f654a14857f8a4b6ab8818ebdc9fa13be2309f70
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-03 19:01:14 +0400

    Day3: Part 2

--- Day-3-Gear-Ratios/main.c
+++ Day-3-Gear-Ratios/main.c
@@ -6,6 +6,22 @@
 char a[MAXN][MAXN];
 unsigned n = 0, m = 0;
 
+struct gear {
+    int count;
+    int product;
+};
+
+struct gear gears[MAXN * MAXN];
+
+void init_gears() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            gears[i * m + j].count = 0;
+            gears[i * m + j].product = 1;
+        }
+    }
+}
+
 int is_digit(char c) {
     return c >= '0' && c <= '9';
 }
@@ -23,19 +39,28 @@ int to_int(int row, int start, int end) {
     return result;
 }
 
-int is_symbol(int i, int j) {
+void inject_gear(int i, int j, int value) {
+    int index = i * m + j;
+    gears[index].count++;
+    gears[index].product *= value;
+}
+
+int is_symbol(int i, int j, int value) {
     if (i < 0 || i >= n || j < 0 || j >= m) {
         return 0;
     }
     char c = a[i][j];
+    if (c == '*') {
+        inject_gear(i, j, value);
+    }
     return c != '.' && c != '\n';
 }
 
 int calculate_for_number(int row, int start, int end) {
     int number = to_int(row, start, end);
-    int symbol_count = is_symbol(row, start - 1) + is_symbol(row, end);
+    int symbol_count = is_symbol(row, start - 1, number) + is_symbol(row, end, number);
     for (int j = start - 1; j <= end; j++) {
-        symbol_count += is_symbol(row - 1, j) + is_symbol(row + 1, j);
+        symbol_count += is_symbol(row - 1, j, number) + is_symbol(row + 1, j, number);
     }
     return symbol_count * number;
 }
@@ -58,6 +83,16 @@ int solve() {
     return result;
 }
 
+int get_gears_result() {
+    int result = 0;
+    for (int i = 0, ei = n * m; i < ei; i++) {
+        if (gears[i].count == 2) {
+            result += gears[i].product;
+        }
+    }
+    return result;
+}
+
 int main(int argc, char** argv) {
     FILE* fptr = fopen(argv[1], "r");
     while (fgets(a[n], MAXN, fptr)) {
@@ -66,8 +101,11 @@ int main(int argc, char** argv) {
         }
         n++;
     }
+    init_gears();
     int result = solve();
     printf("%d\n", result);
+    int gear_ratios = get_gears_result();
+    printf("%d\n", gear_ratios);
     fclose(fptr);
     return 0;
 }
