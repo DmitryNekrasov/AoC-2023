bdea4bd4055439e479bfb30f358d57e3e5c32375
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-03 18:44:44 +0400

    Day3: Part 1

--- Day-3-Gear-Ratios/main.c
+++ Day-3-Gear-Ratios/main.c
@@ -4,6 +4,7 @@
 #define MAXN 200
 
 char a[MAXN][MAXN];
+unsigned n = 0, m = 0;
 
 int is_digit(char c) {
     return c >= '0' && c <= '9';
@@ -22,7 +23,25 @@ int to_int(int row, int start, int end) {
     return result;
 }
 
-void scan(int n, int m) {
+int is_symbol(int i, int j) {
+    if (i < 0 || i >= n || j < 0 || j >= m) {
+        return 0;
+    }
+    char c = a[i][j];
+    return c != '.' && c != '\n';
+}
+
+int calculate_for_number(int row, int start, int end) {
+    int number = to_int(row, start, end);
+    int symbol_count = is_symbol(row, start - 1) + is_symbol(row, end);
+    for (int j = start - 1; j <= end; j++) {
+        symbol_count += is_symbol(row - 1, j) + is_symbol(row + 1, j);
+    }
+    return symbol_count * number;
+}
+
+int solve() {
+    int result = 0;
     for (int i = 0; i < n; i++) {
         int start = -1;
         for (int j = 0; j < m; j++) {
@@ -31,24 +50,24 @@ void scan(int n, int m) {
                     start = j;
                 }
             } else if (start != -1) {
-                int result = to_int(i, start, j);
-                printf("result = %d\n", result);
+                result += calculate_for_number(i, start, j);
                 start = -1;
             }
         }
     }
+    return result;
 }
 
 int main(int argc, char** argv) {
     FILE* fptr = fopen(argv[1], "r");
-    unsigned n = 0, m = 0;
     while (fgets(a[n], MAXN, fptr)) {
         if (m == 0) {
             m = strlen(a[n]);
         }
         n++;
     }
-    scan(n, m);
+    int result = solve();
+    printf("%d\n", result);
     fclose(fptr);
     return 0;
 }
