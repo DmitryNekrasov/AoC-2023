df3989c938d11f85928c884b8ca89d44d3844d13
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-04 16:46:13 +0400

    Day 4: Part 1

--- Day-4-Scratchcards/src/Main.java
+++ Day-4-Scratchcards/src/Main.java
@@ -1,9 +1,65 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
+    private Set<Integer> stringNumbersToSet(String str) {
+        return Set.copyOf(stringNumbersToList(str));
+    }
+    
+    private List<Integer> stringNumbersToList(String str) {
+        List<Integer> numbers = new ArrayList<>();
+        var st = new StringTokenizer(str, " ");
+        while (st.hasMoreTokens()) {
+            numbers.add(Integer.parseInt(st.nextToken()));
+        }
+        return numbers;
+    }
+
+    private int winningNumbersCount(Set<Integer> haystack, List<Integer> needle) {
+        int result = 0;
+        for (var num : needle) {
+            if (haystack.contains(num)) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    private int pow2(int n) {
+        if (n < 0) return 0;
+        int result = 1;
+        for (int i = 0; i < n; i++) {
+            result <<= 1;
+        }
+        return result;
+    }
+
+    private int solveLine(String line) {
+        var st = new StringTokenizer(line, ":|");
+        st.nextToken();
+        var winningNumbers = stringNumbersToSet(st.nextToken());
+        var myNumbers = stringNumbersToList(st.nextToken());
+        int winningCount = winningNumbersCount(winningNumbers, myNumbers);
+        return pow2(winningCount - 1);
+    }
+    private void solve() throws IOException {
+        String line = in.readLine();
+        int ans = 0;
+        while (line != null) {
+            ans += solveLine(line);
+            line = in.readLine();
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) throws IOException {
         new Main().run();
     }
 
-    private void run() {
-        System.out.println("Hello!");
+    private BufferedReader in;
+    private void run() throws IOException {
+        in = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
+        solve();
+        in.close();
     }
 }
