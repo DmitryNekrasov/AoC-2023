e97d896e0f7e73c6830e0375fd3d1528acbda2ee
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-06 15:46:14 +0400

    Day 6: Initial

---
+++ Day-6-Wait-For-It/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAXN 100
+
+FILE* fptr;
+
+int get_values(int* result) {
+    char buf[MAXN];
+    fgets(buf, MAXN, fptr);
+    char* token = strtok(buf, " ");
+    token = strtok(NULL, " ");
+    int index = 0;
+    while (token) {
+        int value = atoi(token);
+        result[index] = value;
+        index++;
+        token = strtok(NULL, " ");
+    }
+    return index;
+}
+
+int main(int argc, char** argv) {
+    fptr = fopen(argv[1], "r");
+    int times[MAXN], distances[MAXN];
+    get_values(times);
+    int size = get_values(distances);
+
+    return 0;
+}
