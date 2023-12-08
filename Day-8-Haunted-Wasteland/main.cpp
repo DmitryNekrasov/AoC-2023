ed9fbac4effca40af4b552ea037c5265ba42724b
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-08 15:23:11 +0400

    Day 8: Part 1

--- Day-8-Haunted-Wasteland/main.cpp
+++ Day-8-Haunted-Wasteland/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <unordered_map>
 
 using namespace std;
+using Graph = unordered_map<string, pair<string, string>>;
 
 pair<string , pair<string, string>> parse_line(const string& line) {
     string from, left, right, skip;
@@ -11,16 +13,29 @@ pair<string , pair<string, string>> parse_line(const string& line) {
     return make_pair(from, make_pair(left.substr(1, 3), right.substr(0, 3)));
 }
 
+int solve(const string& direction, const Graph& graph) {
+    string current = "AAA";
+    int direction_index = 0, n = direction.length(), step_count = 0;
+    while (current != "ZZZ") {
+        current = direction[direction_index % n] == 'L' ? graph.at(current).first : graph.at(current).second;
+        direction_index++;
+        step_count++;
+    }
+    return step_count;
+}
+
 int main(int argc, char** argv) {
     ifstream ifs(argv[1]);
     string direction;
     getline(ifs, direction);
     string line;
     getline(ifs, line);
+    Graph graph;
     while (getline(ifs, line)) {
         auto res = parse_line(line);
-        cout << res.first << " " << res.second.first << " " << res.second.second << endl;
+        graph[res.first] = res.second;
     }
-
+    int ans_part_one = solve(direction, graph);
+    cout << ans_part_one << endl;
     return 0;
 }
