4b3eb4499d62353ece8737acab3b7106c51f28eb
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-08 16:43:56 +0400

    Day 8: Refactoring

--- Day-8-Haunted-Wasteland/main.cpp
+++ Day-8-Haunted-Wasteland/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <unordered_map>
+#include <queue>
 
 using namespace std;
 using Graph = unordered_map<string, pair<string, string>>;
@@ -13,15 +14,18 @@ pair<string , pair<string, string>> parse_line(const string& line) {
     return make_pair(from, make_pair(left.substr(1, 3), right.substr(0, 3)));
 }
 
-int solve(const string& direction, const Graph& graph) {
-    string current = "AAA";
-    int direction_index = 0, n = direction.length(), step_count = 0;
-    while (current != "ZZZ") {
-        current = direction[direction_index % n] == 'L' ? graph.at(current).first : graph.at(current).second;
-        direction_index++;
+int solve_part_1(const string& direction, const Graph& graph, const string& start, const string& end) {
+    string current = start;
+    int n = direction.length(), step_count = 0;
+    do {
+        current = direction[step_count % n] == 'L' ? graph.at(current).first : graph.at(current).second;
         step_count++;
-    }
+    } while (current != end);
     return step_count;
+}
+
+int solve_part_2(const string& direction, const Graph& graph) {
+    return -1;
 }
 
 int main(int argc, char** argv) {
@@ -32,10 +36,10 @@ int main(int argc, char** argv) {
     getline(ifs, line);
     Graph graph;
     while (getline(ifs, line)) {
-        auto res = parse_line(line);
-        graph[res.first] = res.second;
+        const auto [key, pair] = parse_line(line);
+        graph[key] = pair;
     }
-    int ans_part_one = solve(direction, graph);
-    cout << ans_part_one << endl;
+    cout << solve_part_1(direction, graph, "AAA", "ZZZ") << endl;
+    cout << solve_part_2(direction, graph) << endl;
     return 0;
 }
