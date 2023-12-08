c6a767e6e1257352406ee047448998d441e8b3e3
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-08 15:01:53 +0400

    Day 8: Add parsing

--- Day-8-Haunted-Wasteland/main.cpp
+++ Day-8-Haunted-Wasteland/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+pair<string , pair<string, string>> parse_line(const string& line) {
+    string from, left, right, skip;
+    stringstream ss(line);
+    ss >> from >> skip >> left >> right;
+    return make_pair(from, make_pair(left.substr(1, 3), right.substr(0, 3)));
+}
+
+int main(int argc, char** argv) {
+    ifstream ifs(argv[1]);
+    string direction;
+    getline(ifs, direction);
+    string line;
+    getline(ifs, line);
+    while (getline(ifs, line)) {
+        auto res = parse_line(line);
+        cout << res.first << " " << res.second.first << " " << res.second.second << endl;
+    }
+
     return 0;
 }
