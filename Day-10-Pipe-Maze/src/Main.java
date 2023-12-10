cba5ce7316a267d4106d3f5703a02333e5eb4a65
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-10 15:38:52 +0400

    Day 10: Output maze

--- Day-10-Pipe-Maze/src/Main.java
+++ Day-10-Pipe-Maze/src/Main.java
@@ -1,5 +1,33 @@
+import java.io.*;
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello!");
+    private final static Map<Character, Character> mapping =
+            Map.of('F', '┌', '7', '┐', 'J', '┘', 'L', '└', '|', '│', '-', '─');
+
+    private void solve() throws IOException {
+        String line = in.readLine();
+        while (line != null) {
+            System.out.println(
+                    line.chars()
+                            .map(c -> mapping.getOrDefault((char) c, ' '))
+                            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                            .toString()
+            );
+
+            line = in.readLine();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().run();
+    }
+
+    private BufferedReader in;
+
+    private void run() throws IOException {
+        in = new BufferedReader(new InputStreamReader(new FileInputStream("input_simple_2.txt")));
+        solve();
+        in.close();
     }
 }
