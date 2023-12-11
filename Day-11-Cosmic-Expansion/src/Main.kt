a6e6679d19dcb471e993d24af8ef0a21fae4ac9b
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-11 16:05:46 +0400

    Day 11: Add toGalaxyList

--- Day-11-Cosmic-Expansion/src/Main.kt
+++ Day-11-Cosmic-Expansion/src/Main.kt
@@ -1,4 +1,9 @@
 import java.io.File
+import kotlin.math.abs
+
+data class Galaxy(val i: Int, val j: Int) {
+    fun distance(o: Galaxy) = abs(i - o.i) + abs(j - o.j)
+}
 
 fun List<String>.getEmptyRows() = this.withIndex().filter { (_, row) -> row.all { it == '.' } }.map { it.index }.toSet()
 
@@ -18,12 +23,18 @@ fun List<String>.getEmptyCols(): Set<Int> {
     return result
 }
 
+fun List<String>.toGalaxyList() = this.withIndex()
+    .map { (i, row) -> row.withIndex().filter { (_, c) -> c == '#' }.map { Galaxy(i, it.index) } }
+    .flatten()
+
 fun solve(image: List<String>): Int {
     val emptyRows = image.getEmptyRows()
     val emptyCols = image.getEmptyCols()
+    val galaxyList = image.toGalaxyList()
 
     println(emptyRows)
     println(emptyCols)
+    println(galaxyList)
 
     return -1
 }
