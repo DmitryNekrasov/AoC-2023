6d1b695de323ab2fffd727f2d7520a16e34a51c9
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-11 15:48:33 +0400

    Day 11: Add getEmptyRows, getEmptyCols

--- Day-11-Cosmic-Expansion/src/Main.kt
+++ Day-11-Cosmic-Expansion/src/Main.kt
@@ -1,7 +1,35 @@
 import java.io.File
 
+fun List<String>.getEmptyRows() = this.withIndex().filter { (_, row) -> row.all { it == '.' } }.map { it.index }.toSet()
+
+fun List<String>.getEmptyCols(): Set<Int> {
+    val result = HashSet<Int>()
+    val n = size
+    val m = first().length
+    for (j in 0..<m) {
+        var colIsEmpty = true
+        for (i in 0..<n) {
+            colIsEmpty = colIsEmpty && this[i][j] == '.'
+        }
+        if (colIsEmpty) {
+            result.add(j)
+        }
+    }
+    return result
+}
+
+fun solve(image: List<String>): Int {
+    val emptyRows = image.getEmptyRows()
+    val emptyCols = image.getEmptyCols()
+
+    println(emptyRows)
+    println(emptyCols)
+
+    return -1
+}
+
 fun main() {
     val image = File("input_simple_1.txt").useLines { it.toList() }
-    image.joinToString("\n")
-        .also { println(it) }
+    val ansPartOne = solve(image)
+    println(ansPartOne)
 }
