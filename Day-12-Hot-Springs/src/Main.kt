4012f1455f2dc167b621629b2c722e09d9d7922f
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-12 18:00:53 +0400

    Day 12: Parse data

--- Day-12-Hot-Springs/src/Main.kt
+++ Day-12-Hot-Springs/src/Main.kt
@@ -1,3 +1,17 @@
+import java.io.File
+
+fun solve(spring: String, record: List<Int>): Int {
+    println("$spring, $record")
+
+    return -1
+}
+
 fun main() {
-    println("Hello!")
+    var ansPartOne = 0
+    for (line in File("input.txt").useLines { it.toList() }) {
+        val (spring, recordString) = line.split(" ")
+        val record = recordString.split(",").map { it.toInt() }
+        ansPartOne += solve(spring, record)
+    }
+    println(ansPartOne)
 }
