7d5ae14ed51e6d5c59a82a7308814d24a7d6454f
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-12 20:42:34 +0400

    Day 12: Part 2

--- Day-12-Hot-Springs/src/Main.kt
+++ Day-12-Hot-Springs/src/Main.kt
@@ -21,12 +21,15 @@ fun solvePartOne(spring: String, record: List<Int>): Int {
 
 fun solve(initialSpring: String, initialRecord: List<Int>, times: Int): Long {
     val spring = List(times) { initialSpring }.joinToString("?") + "."
-    val record= List(times) { initialRecord }.flatten()
-
+    val record = List(times) { initialRecord }.flatten()
+    val cache = HashMap<Triple<Int, Int, Int>, Long>()
     fun calc(springIndex: Int, recordIndex: Int, hashLen: Int): Long {
         if (springIndex == spring.length && recordIndex == record.size) return 1L
         if (springIndex == spring.length) return 0L
-
+        val triple = Triple(springIndex, recordIndex, hashLen)
+        if (triple in cache) {
+            return cache[triple]!!
+        }
         if (spring[springIndex] == '#') return calc(springIndex + 1, recordIndex, hashLen + 1)
         if (spring[springIndex] == '.') {
             if (hashLen == 0) {
@@ -36,7 +39,6 @@ fun solve(initialSpring: String, initialRecord: List<Int>, times: Int): Long {
                 return if (record[recordIndex] == hashLen) calc(springIndex + 1, recordIndex + 1, 0) else 0L
             }
         }
-
         // Put dot
         var result = 0L
         if (hashLen == 0) {
@@ -45,23 +47,22 @@ fun solve(initialSpring: String, initialRecord: List<Int>, times: Int): Long {
             if (recordIndex == record.size) return 0L
             result += if (hashLen == record[recordIndex]) calc(springIndex + 1, recordIndex + 1, 0) else 0L
         }
-
         // Put hash
-        result += calc(springIndex + 1, recordIndex, hashLen + 1);
-
+        result += calc(springIndex + 1, recordIndex, hashLen + 1)
+        cache[triple] = result
         return result;
     }
     return calc(0, 0, 0)
 }
 
 fun main() {
-    var ansPartOne = 0
+    var ansPartOne = 0L
     var ansPartTwo = 0L
     for (line in File("input.txt").useLines { it.toList() }) {
         val (spring, recordString) = line.split(" ")
         val record = recordString.split(",").map { it.toInt() }
-        ansPartOne += solvePartOne(spring, record)
-        ansPartTwo += solve(spring, record, 1)
+        ansPartOne += solve(spring, record, 1)
+        ansPartTwo += solve(spring, record, 5)
     }
     println(ansPartOne)
     println(ansPartTwo)
