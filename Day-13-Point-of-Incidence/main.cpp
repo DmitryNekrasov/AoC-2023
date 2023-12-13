1ce67489d2bb636c7106d78128d6f0cb17233747
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-13 17:42:00 +0400

    Day 13: Part 2

--- Day-13-Point-of-Incidence/main.cpp
+++ Day-13-Point-of-Incidence/main.cpp
@@ -69,14 +69,34 @@ pair<int, int> solve_part_one(const vector<vector<char>>& field, int last_vertic
     return make_pair(vertical, horizontal);
 }
 
+char reverse(char c) {
+    return c == '#' ? '.' : '#';
+}
+
+int solve_part_two(vector<vector<char>>& field, int last_vertical = 0, int last_horizontal = 0) {
+    size_t n = field.size(), m = field.front().size();
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < m; j++) {
+            field[i][j] = reverse(field[i][j]);
+            auto [vertical, horizontal] = solve_part_one(field, last_vertical, last_horizontal);
+            if (vertical != 0) return vertical;
+            if (horizontal != 0) return 100 * horizontal;
+            field[i][j] = reverse(field[i][j]);
+        }
+    }
+    cout << "SHOULD_NOT_REACH_HERE" << endl;
+    return -1;
+}
+
 int main(int argc, char** argv) {
     ifstream ifs(argv[1]);
     string line;
     vector<vector<char>> field;
-    int ans_part_one = 0;
-    auto calc = [&field, &ans_part_one]() {
+    int ans_part_one = 0, ans_part_two = 0;
+    auto calc = [&field, &ans_part_one, &ans_part_two]() {
         auto [vertical, horizontal] = solve_part_one(field);
         ans_part_one += vertical + 100 * horizontal;
+        ans_part_two += solve_part_two(field, vertical, horizontal);
         field.clear();
     };
     while (getline(ifs, line)) {
@@ -89,5 +109,6 @@ int main(int argc, char** argv) {
     }
     calc();
     cout << ans_part_one << endl;
+    cout << ans_part_two << endl;
     return 0;
 }
