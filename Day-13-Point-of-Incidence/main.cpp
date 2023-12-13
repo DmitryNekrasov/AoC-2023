a9eb6822814ec767928da685aaf4380eda2942b6
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-13 14:38:10 +0400

    Day 13: Parse data

--- Day-13-Point-of-Incidence/main.cpp
+++ Day-13-Point-of-Incidence/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+void solve(const vector<string>& field) {
+    for (const auto& row : field) {
+        cout << row << endl;
+    }
+    cout << endl;
+}
+
+int main(int argc, char** argv) {
+    ifstream ifs(argv[1]);
+    string line;
+    vector<string> field;
+    while (getline(ifs, line)) {
+        if (line.empty()) {
+            solve(field);
+            field.clear();
+        } else {
+            field.push_back(line);
+        }
+    }
+    solve(field);
+
     return 0;
 }
