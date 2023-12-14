45b1fcd543357c217c8ea39f423759dad2f1590c
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-14 14:06:03 +0400

    Day 14: Parse data

--- Day-14-Parabolic-Reflector-Dish/src/Main.java
+++ Day-14-Parabolic-Reflector-Dish/src/Main.java
@@ -2,15 +2,44 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    private void solve() throws IOException {
-        String line = in.readLine();
-        while (line != null) {
-            System.out.println(line);
-            line = in.readLine();
+    private void solvePartOne(char[][] grid) {
+        printGrid(grid);
+    }
+
+    private char[][] asCharArray(List<String> gridList) {
+        int n = gridList.size(), m = gridList.getFirst().length();
+        char[][] grid = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            System.arraycopy(gridList.get(i).toCharArray(), 0, grid[i], 0, m);
+        }
+        return grid;
+    }
+
+    private void printGrid(char[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(grid[i][j]);
+            }
+            System.out.println();
         }
     }
+
+    private void solve() throws IOException {
+        String line = in.readLine();
+        var gridList = new ArrayList<String>();
+        while (line != null) {
+            gridList.add(line);
+            line = in.readLine();
+        }
+        var grid = asCharArray(gridList);
+        solvePartOne(grid);
+    }
+
     public static void main(String[] args) throws IOException {
         new Main().run();
     }
@@ -18,7 +47,7 @@ public class Main {
     private BufferedReader in;
 
     private void run() throws IOException {
-        in = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
+        in = new BufferedReader(new InputStreamReader(new FileInputStream("input_simple.txt")));
         solve();
         in.close();
     }
