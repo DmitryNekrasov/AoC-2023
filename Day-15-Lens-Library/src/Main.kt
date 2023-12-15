49e325e707d0c9f8d66b34190e0dfb25f5b77b41
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-15 14:25:48 +0400

    Day 15: Part 1

--- Day-15-Lens-Library/src/Main.kt
+++ Day-15-Lens-Library/src/Main.kt
@@ -1,12 +1,14 @@
 import java.io.File
 
-fun solvePartOne(steps: List<String>): Int {
-    println(steps)
-    return -1
-}
+const val BASE = 17
+const val MOD = 256
+
+fun String.hash() = this.fold(0) { acc, c -> (acc + c.code) * BASE % MOD }
+
+fun solvePartOne(steps: List<String>) = steps.sumOf { it.hash() }
 
 fun main() {
-    val steps = File("input_simple.txt").useLines { it.toList() }.first().split(",")
+    val steps = File("input.txt").useLines { it.toList() }.first().split(",")
     val ansPartOne = solvePartOne(steps)
     println(ansPartOne)
 }
