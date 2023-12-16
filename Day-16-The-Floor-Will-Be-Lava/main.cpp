a0e392023e4168b389aad8c1899f6d9737903e3c
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-16 18:50:18 +0400

    Day 16: Part 1

--- Day-16-The-Floor-Will-Be-Lava/main.cpp
+++ Day-16-The-Floor-Will-Be-Lava/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <queue>
 
 using namespace std;
+
+static constexpr int RIGHT = 1;
+static constexpr int DOWN = 2;
+static constexpr int LEFT = 4;
+static constexpr int UP = 8;
 
 void print_field(const vector<string>& field) {
     for (const auto& line : field) {
@@ -10,9 +16,77 @@ void print_field(const vector<string>& field) {
     }
 }
 
+int energized_number(const vector<vector<int>>& visited) {
+    int result = 0;
+    for (const auto& row : visited) {
+        for (int value : row) {
+            result += value != 0;
+        }
+    }
+    return result;
+}
+
 int solve_port_one(const vector<string>& field) {
-    print_field(field);
-    return -1;
+    size_t n = field.size(), m = field.front().length();
+    vector<vector<int>> visited(n, vector<int>(m, 0));
+    queue<tuple<int, int, int>> q;
+    q.emplace(0, 0, RIGHT);
+    while (!q.empty()) {
+        auto [i, j, direction] = q.front();
+        q.pop();
+        if (i < 0 || i >= n || j < 0 || j >= m) continue;
+        if ((visited[i][j] & direction) != 0) continue;
+        visited[i][j] |= direction;
+        switch (field[i][j]) {
+            case '/':
+                switch (direction) {
+                    case RIGHT: q.emplace(i - 1, j, UP); break;
+                    case DOWN: q.emplace(i, j - 1, LEFT); break;
+                    case LEFT: q.emplace(i + 1, j, DOWN); break;
+                    case UP: q.emplace(i, j + 1, RIGHT); break;
+                    default: cout << "SHOULD_NOT_REACH_HERE" << endl;
+                }
+                break;
+            case '\\':
+                switch (direction) {
+                    case RIGHT: q.emplace(i + 1, j, DOWN); break;
+                    case DOWN: q.emplace(i, j + 1, RIGHT); break;
+                    case LEFT: q.emplace(i - 1, j, UP); break;
+                    case UP: q.emplace(i, j - 1, LEFT); break;
+                    default: cout << "SHOULD_NOT_REACH_HERE" << endl;
+                }
+                break;
+            case '-':
+                switch (direction) {
+                    case DOWN:
+                    case UP: q.emplace(i, j - 1, LEFT); q.emplace(i, j + 1, RIGHT); break;
+                    case RIGHT: q.emplace(i, j + 1, RIGHT); break;
+                    case LEFT: q.emplace(i, j - 1, LEFT); break;
+                    default: cout << "SHOULD_NOT_REACH_HERE" << endl;
+                }
+                break;
+            case '|':
+                switch (direction) {
+                    case RIGHT:
+                    case LEFT: q.emplace(i - 1, j, UP); q.emplace(i + 1, j, DOWN); break;
+                    case DOWN: q.emplace(i + 1, j, DOWN); break;
+                    case UP: q.emplace(i - 1, j, UP); break;
+                    default: cout << "SHOULD_NOT_REACH_HERE" << endl;
+                }
+                break;
+            case '.':
+                switch (direction) {
+                    case RIGHT: q.emplace(i, j + 1, RIGHT); break;
+                    case DOWN: q.emplace(i + 1, j, DOWN); break;
+                    case LEFT: q.emplace(i, j - 1, LEFT); break;
+                    case UP: q.emplace(i - 1, j, UP); break;
+                    default: cout << "SHOULD_NOT_REACH_HERE" << endl;
+                }
+                break;
+            default: cout << "SHOULD_NOT_REACH_HERE" << endl;
+        }
+    }
+    return energized_number(visited);
 }
 
 int main(int argc, char** argv) {
