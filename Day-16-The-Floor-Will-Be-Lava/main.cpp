10714a2c2d89f11b6930463df5bdeb096df3df7e
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-16 14:57:17 +0400

    Day 16: Parse data

--- Day-16-The-Floor-Will-Be-Lava/main.cpp
+++ Day-16-The-Floor-Will-Be-Lava/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+void print_field(const vector<string>& field) {
+    for (const auto& line : field) {
+        cout << line << endl;
+    }
+}
+
+int solve_port_one(const vector<string>& field) {
+    print_field(field);
+    return -1;
+}
+
+int main(int argc, char** argv) {
+    ifstream ifs(argv[1]);
+    string line;
+    vector<string> field;
+    while (getline(ifs, line)) {
+        field.push_back(line);
+    }
+    int ans_part_one = solve_port_one(field);
+    cout << ans_part_one << endl;
     return 0;
 }
