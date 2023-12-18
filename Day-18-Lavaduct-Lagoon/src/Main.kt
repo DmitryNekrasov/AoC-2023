4967e6736decfb2df697dd32f7587f13d24bb417
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-18 13:47:12 +0400

    Day 18: Add calcSizeAndStart

--- Day-18-Lavaduct-Lagoon/src/Main.kt
+++ Day-18-Lavaduct-Lagoon/src/Main.kt
@@ -1,9 +1,35 @@
 import java.io.File
+import kotlin.math.*
 
 data class Edge(val direction: Char, val length: Int, val color: String)
 
+fun calcSizeAndStart(edges: List<Edge>): IntArray {
+    var minI = Int.MAX_VALUE
+    var minJ = Int.MAX_VALUE
+    var maxI = 0
+    var maxJ = 0
+    var i = 0
+    var j = 0
+    for (edge in edges) {
+        when (edge.direction) {
+            'R' -> j += edge.length
+            'L' -> j -= edge.length
+            'D' -> i += edge.length
+            'U' -> i -= edge.length
+            else -> throw RuntimeException()
+        }
+        minI = min(minI, i)
+        minJ = min(minJ, j)
+        maxI = max(maxI, i)
+        maxJ = max(maxJ, j)
+    }
+    return intArrayOf(maxI - minI + 1, maxJ - minJ + 1, -minI, -minJ)
+}
+
 fun solvePartOne(edges: List<Edge>): Int {
     println(edges)
+    val (n, m, startI, startJ) = calcSizeAndStart(edges)
+    println("n = $n, m = $m, startI = $startI, startJ = $startJ")
 
     return -1
 }
