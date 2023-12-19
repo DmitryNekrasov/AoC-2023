2dfb7410bace32737906d266f4083b9279ae856c
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-19 18:19:28 +0400

    Day 19: Part 2

--- Day-19-Aplenty/src/Main.kt
+++ Day-19-Aplenty/src/Main.kt
@@ -1,4 +1,6 @@
 import java.io.File
+import java.util.LinkedList
+import java.util.Queue
 
 data class Part(val x: Int, val m: Int, val a: Int, val s: Int) {
     val rating: Int
@@ -13,7 +15,29 @@ data class Part(val x: Int, val m: Int, val a: Int, val s: Int) {
     }
 }
 
-data class PartRange(val xRange: IntRange, val mRange: IntRange, val aRange: IntRange, val sRange: IntRange)
+data class PartRange(val xRange: IntRange, val mRange: IntRange, val aRange: IntRange, val sRange: IntRange) {
+    val number: Long
+        get() = (xRange.last - xRange.first + 1).toLong() *
+                (mRange.last - mRange.first + 1).toLong() *
+                (aRange.last - aRange.first + 1).toLong() *
+                (sRange.last - sRange.first + 1).toLong()
+
+    fun splitBy(x: Char, value: Int): Pair<PartRange, PartRange> {
+        return when (x) {
+            'x' -> PartRange(xRange.first..<value, mRange, aRange, sRange) to PartRange(value..xRange.last, mRange, aRange, sRange)
+            'm' -> PartRange(xRange, mRange.first..<value, aRange, sRange) to PartRange(xRange, value..mRange.last, aRange, sRange)
+            'a' -> PartRange(xRange, mRange, aRange.first..<value, sRange) to PartRange(xRange, mRange, value..aRange.last, sRange)
+            's' -> PartRange(xRange, mRange, aRange, sRange.first..<value) to PartRange(xRange, mRange, aRange, value..sRange.last)
+            else -> throw RuntimeException()
+        }
+    }
+
+    companion object {
+        private const val LIMIT = 4000
+        val INITIAL_RANGE = PartRange(1..LIMIT, 1..LIMIT, 1..LIMIT, 1..LIMIT)
+        val EMPTY_RANGE = PartRange(0..0, 0..0, 0..0, 0..0)
+    }
+}
 
 data class Workflow(val rules: List<Rule>) {
     fun lastState(part: Part): String {
@@ -24,18 +48,28 @@ data class Workflow(val rules: List<Rule>) {
         }
         throw RuntimeException()
     }
+
+    fun nextRanges(partRange: PartRange): List<Pair<String, PartRange>> {
+        val result = mutableListOf<Pair<String, PartRange>>()
+        var range = partRange
+        for (rule in rules) {
+            val (trueRange, falseRange) = rule.splitRange(range)
+            if (trueRange != PartRange.EMPTY_RANGE) {
+                result.add(rule.nextState to trueRange)
+            }
+            range = falseRange
+        }
+        return result
+    }
 }
 
 abstract class Rule(val nextState: String) {
     abstract fun isAccepted(part: Part): Boolean
+    protected abstract fun isAccepted(partRange: PartRange): Boolean
+    abstract fun splitRange(partRange: PartRange): Pair<PartRange, PartRange> // true, false
 }
 
-class RuleWithCondition(
-    private val category: Char,
-    private val comparisonSign: Char,
-    private val value: Int,
-    nextState: String
-) : Rule(nextState) {
+class RuleWithCondition(private val category: Char, private val comparisonSign: Char, private val value: Int, nextState: String) : Rule(nextState) {
     override fun isAccepted(part: Part): Boolean {
         val x = when (category) {
             'x' -> part.x
@@ -47,12 +81,30 @@ class RuleWithCondition(
         return if (comparisonSign == '>') x > value else x < value
     }
 
+    override fun isAccepted(partRange: PartRange): Boolean {
+        return when (category) {
+            'x' -> value in partRange.xRange
+            'm' -> value in partRange.mRange
+            'a' -> value in partRange.aRange
+            's' -> value in partRange.sRange
+            else -> throw RuntimeException()
+        }
+    }
+
+    override fun splitRange(partRange: PartRange): Pair<PartRange, PartRange> {
+        if (!isAccepted(partRange)) return PartRange.EMPTY_RANGE to partRange
+        if (comparisonSign == '<') return partRange.splitBy(category, value)
+        val (r1, r2) = partRange.splitBy(category, value + 1)
+        return r2 to r1
+    }
+
     override fun toString() = "$category$comparisonSign$value:$nextState"
 }
 
 class AlwaysTrueRule(nextState: String) : Rule(nextState) {
     override fun isAccepted(part: Part) = true
-
+    override fun isAccepted(partRange: PartRange) = true
+    override fun splitRange(partRange: PartRange) = partRange to PartRange.EMPTY_RANGE
     override fun toString() = nextState
 }
 
@@ -63,10 +115,7 @@ fun parseWorkflowStrings(workflowStrings: List<String>): Map<String, Workflow> {
                 if (':' in ruleString) {
                     val (condition, nextWorkflow) = ruleString.split(":")
                     return RuleWithCondition(
-                        condition[0],
-                        condition[1],
-                        condition.substring(2, condition.length).toInt(),
-                        nextWorkflow
+                        condition[0], condition[1], condition.substring(2, condition.length).toInt(), nextWorkflow
                     )
                 }
                 return AlwaysTrueRule(ruleString)
@@ -81,9 +130,7 @@ fun parseWorkflowStrings(workflowStrings: List<String>): Map<String, Workflow> {
 
 fun parsePartStrings(partStrings: List<String>): List<Part> {
     fun parsePartString(partString: String): Part {
-        val (x, m, a, s) = partString.substring(1..<partString.lastIndex)
-            .split(",")
-            .map { it.split("=").last() }
+        val (x, m, a, s) = partString.substring(1..<partString.lastIndex).split(",").map { it.split("=").last() }
             .map { it.toInt() }
         return Part(x, m, a, s)
     }
@@ -93,8 +140,21 @@ fun parsePartStrings(partStrings: List<String>): List<Part> {
 fun solvePartOne(workflows: Map<String, Workflow>, parts: List<Part>): Int {
     return parts.filter { it.isAcceptedByWorkflows(workflows) }.sumOf { it.rating }
 }
+
 fun solvePartTwo(workflows: Map<String, Workflow>): Long {
-    return -1L
+    val queue: Queue<Pair<String, PartRange>> = LinkedList()
+    queue.offer("in" to PartRange.INITIAL_RANGE)
+    var ans = 0L
+    while (queue.isNotEmpty()) {
+        val (wfName, range) = queue.poll()
+        if (wfName == "R") continue
+        if (wfName == "A") {
+            ans += range.number
+            continue
+        }
+        queue.addAll(workflows[wfName]!!.nextRanges(range))
+    }
+    return ans
 }
 
 fun main() {
@@ -105,4 +165,5 @@ fun main() {
     val workflows = parseWorkflowStrings(workflowStrings)
     val parts = parsePartStrings(partStrings)
     println(solvePartOne(workflows, parts))
+    println(solvePartTwo(workflows))
 }
