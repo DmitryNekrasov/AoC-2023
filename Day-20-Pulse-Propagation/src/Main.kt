7550640a6073ec76ec2a52715f062bfc673649a6
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-20 16:10:50 +0400

    Day 20: Parse data

--- Day-20-Pulse-Propagation/src/Main.kt
+++ Day-20-Pulse-Propagation/src/Main.kt
@@ -1,3 +1,31 @@
+import java.io.File
+
+enum class ModuleType { FlipFlop, Conjunction, Broadcaster, Terminal }
+
+fun getNameAndType(vertex: String): Pair<String, ModuleType> {
+    return when (vertex[0]) {
+        '%' -> vertex.drop(1) to ModuleType.FlipFlop
+        '&' -> vertex.drop(1) to ModuleType.Conjunction
+        else -> vertex to if (vertex == "broadcaster") ModuleType.Broadcaster else ModuleType.Terminal
+    }
+}
+
+fun parseLine(line: String, vertexType: HashMap<String, ModuleType>): Pair<String, List<String>> {
+    val (from, toString) = line.split(" -> ")
+    val (fromName, fromType) = getNameAndType(from)
+    vertexType[fromName] = fromType
+    val toList = toString.split(", ")
+    for (vertex in toList) {
+        if (vertex !in vertexType) {
+            vertexType[vertex] = ModuleType.Terminal
+        }
+    }
+    return fromName to toList
+}
+
 fun main() {
-    println("Hello!")
+    val vertexType = HashMap<String, ModuleType>()
+    val graph = File("input_simple_2.txt").useLines { it.toList() }.map { parseLine(it, vertexType) }.associateBy({ it.first }, { it.second })
+    println(graph.toList().joinToString("\n") { it.toString() })
+    println(vertexType.toList().joinToString("\n") { it.toString() })
 }
