ea7719f8c8983e1daf3fbb2fff53b4e91291d19c
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-21 14:51:28 +0400

    Day 21: Parse data

--- Day-21-Step-Counter/src/Main.kt
+++ Day-21-Step-Counter/src/Main.kt
@@ -1,3 +1,18 @@
+import java.io.File
+
+fun getStart(field: List<String>) =
+    field.withIndex().filter { (_, line) -> 'S' in line }.map { (i, line) -> i to line.indexOf('S') }.first()
+
+fun solve(field: List<String>): Int {
+    val (startRow, startCol) = getStart(field)
+
+    println(field.joinToString("\n"))
+    println("startRow = $startRow, startCol = $startCol")
+
+    return -1
+}
+
 fun main() {
-    println("Hello!")
+    val field = File("input_simple_1.txt").useLines { it.toList() }
+    println(solve(field))
 }
