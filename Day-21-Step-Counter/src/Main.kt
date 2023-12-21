dc97ddc0be66fd5c648ede49ebd37cef3957dfd3
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-21 15:26:38 +0400

    Day 21: Part 1

--- Day-21-Step-Counter/src/Main.kt
+++ Day-21-Step-Counter/src/Main.kt
@@ -1,18 +1,34 @@
 import java.io.File
+import java.util.LinkedList
+import java.util.Queue
 
-fun getStart(field: List<String>) =
+const val MAX_STEP_NUMBER = 64
+
+fun getStart(field: List<CharArray>) =
     field.withIndex().filter { (_, line) -> 'S' in line }.map { (i, line) -> i to line.indexOf('S') }.first()
 
-fun solve(field: List<String>): Int {
+fun solve(field: List<CharArray>): Int {
+    fun opposite(c: Char) = if (c == 'O') 'E' else 'O'
     val (startRow, startCol) = getStart(field)
-
-    println(field.joinToString("\n"))
-    println("startRow = $startRow, startCol = $startCol")
-
-    return -1
+    val n = field.size
+    val m = field.first().size
+    val queue: Queue<Triple<Int, Int, Int>> = LinkedList()
+    queue.offer(Triple(startRow, startRow, 0))
+    while (queue.isNotEmpty()) {
+        val (i, j, stepCount) = queue.poll()
+        for ((di, dj) in listOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)) {
+            val nextI = i + di
+            val nextJ = j + dj
+            if (nextI in 0..<n && nextJ in 0..<m && field[nextI][nextJ] != '#' && field[nextI][nextJ] == '.' && stepCount < MAX_STEP_NUMBER) {
+                field[nextI][nextJ] = opposite(field[i][j])
+                queue.offer(Triple(nextI, nextJ, stepCount + 1))
+            }
+        }
+    }
+    return 1 + field.sumOf { it.count { it == 'E' } }
 }
 
 fun main() {
-    val field = File("input_simple_1.txt").useLines { it.toList() }
+    val field = File("input.txt").useLines { it.toList() }.map { it.toCharArray() }
     println(solve(field))
 }
