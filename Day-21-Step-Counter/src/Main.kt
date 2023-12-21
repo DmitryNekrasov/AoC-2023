40ed693f030669d6eb3d0609ffce32c0f9d17d58
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-21 16:57:17 +0400

    Day 21: Refactoring

--- Day-21-Step-Counter/src/Main.kt
+++ Day-21-Step-Counter/src/Main.kt
@@ -2,12 +2,11 @@ import java.io.File
 import java.util.LinkedList
 import java.util.Queue
 
-const val MAX_STEP_NUMBER = 64
-
 fun getStart(field: List<CharArray>) =
     field.withIndex().filter { (_, line) -> 'S' in line }.map { (i, line) -> i to line.indexOf('S') }.first()
 
-fun solve(field: List<CharArray>): Int {
+fun solvePartOne(field: List<CharArray>): Int {
+    val limit = 64
     fun opposite(c: Char) = if (c == 'O') 'E' else 'O'
     val (startRow, startCol) = getStart(field)
     val n = field.size
@@ -19,7 +18,7 @@ fun solve(field: List<CharArray>): Int {
         for ((di, dj) in listOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)) {
             val nextI = i + di
             val nextJ = j + dj
-            if (nextI in 0..<n && nextJ in 0..<m && field[nextI][nextJ] != '#' && field[nextI][nextJ] == '.' && stepCount < MAX_STEP_NUMBER) {
+            if (nextI in 0..<n && nextJ in 0..<m && field[nextI][nextJ] != '#' && field[nextI][nextJ] == '.' && stepCount < limit) {
                 field[nextI][nextJ] = opposite(field[i][j])
                 queue.offer(Triple(nextI, nextJ, stepCount + 1))
             }
@@ -30,5 +29,5 @@ fun solve(field: List<CharArray>): Int {
 
 fun main() {
     val field = File("input.txt").useLines { it.toList() }.map { it.toCharArray() }
-    println(solve(field))
+    println(solvePartOne(field))
 }
