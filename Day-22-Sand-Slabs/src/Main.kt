a3b6bbe065a34629107bb62b6c1b37a80f6f105d
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-22 17:38:33 +0400

    Day 22: Part 1

--- Day-22-Sand-Slabs/src/Main.kt
+++ Day-22-Sand-Slabs/src/Main.kt
@@ -1,8 +1,15 @@
 import java.io.File
+import kotlin.math.max
 
 data class Point(val x: Int, val y: Int, val z: Int)
 
-data class Brick(val p1: Point, val p2: Point)
+data class Brick(val p1: Point, val p2: Point) {
+    val over = HashSet<Brick>()
+    val under = HashSet<Brick>()
+
+    infix fun over(brick: Brick) = over.add(brick)
+    infix fun under(brick: Brick) = under.add(brick)
+}
 
 fun parseBrick(brickString: String): Brick {
     fun parsePoint(pointString: String): Point =
@@ -13,19 +20,66 @@ fun parseBrick(brickString: String): Brick {
     return brickString.split("~")
         .let { (p1String, p2String) ->
             Brick(parsePoint(p1String), parsePoint(p2String))
-                .also { brick -> if (brick.p1.z > brick.p2.z) throw RuntimeException() }
-                .also { brick ->
-                    if ((brick.p1.x == brick.p2.x).toInt() +
-                        (brick.p1.y == brick.p2.y).toInt() +
-                        (brick.p1.z == brick.p2.z).toInt() < 2
-                    ) throw RuntimeException().also { println(brick) }
+                .also { (p1, p2) ->
+                    if (p1.x > p2.x ||
+                        p1.y > p2.y ||
+                        p1.z > p2.z
+                    ) throw RuntimeException()
+                }
+                .also { (p1, p2) ->
+                    if ((p1.x == p2.x).toInt() +
+                        (p1.y == p2.y).toInt() +
+                        (p1.z == p2.z).toInt() < 2
+                    ) throw RuntimeException()
                 }
         }
 }
 
+fun solve(bricks: List<Brick>): Int {
+    val (n, m) = bricks.fold(0 to 0) { acc, b ->
+        max(acc.first, max(b.p1.x, b.p2.x)) to
+                max(acc.second, max(b.p1.y, b.p2.y))
+    }.let { it.first + 1 to it.second + 1 }
+    val land = Brick(Point(0, 0, 0), Point(n - 1, m - 1, 0))
+    val a = Array(n) { Array(m) { 0 to land } }
+    for (brick in bricks) {
+        val (p1, p2) = brick
+        if (p1.z != p2.z) {
+            val (x, y) = p1.x to p1.y
+            val len = p2.z - p1.z + 1
+            val (lastHeight, lastBrick) = a[x][y]
+            a[x][y] = lastHeight + len to brick
+            brick over lastBrick
+            lastBrick under brick
+        } else {
+            var maxCurrentHeight = 0
+            for (i in p1.x..p2.x) {
+                for (j in p1.y..p2.y) {
+                    if (a[i][j].first > maxCurrentHeight) {
+                        maxCurrentHeight = a[i][j].first
+                    }
+                }
+            }
+            for (i in p1.x..p2.x) {
+                for (j in p1.y..p2.y) {
+                    val (lastHeight, lastBrick) = a[i][j]
+                    a[i][j] = maxCurrentHeight + 1 to brick
+                    if (lastHeight == maxCurrentHeight) {
+                        brick over lastBrick
+                        lastBrick under brick
+                    }
+                }
+            }
+        }
+    }
+
+    bricks.joinToString("\n") { "$it, over: ${it.over.size}, under: ${it.under.size}" }
+        .also { println(it) }
+
+    return bricks.count { brick -> brick.under.all { it.over.size > 1 } }
+}
+
 fun main() {
     val bricks = File("input.txt").useLines { it.toList() }.map { parseBrick(it) }.sortedBy { it.p1.z }
-
-    bricks.joinToString("\n") { it.toString() }
-        .also { println(it) }
+    println(solve(bricks))
 }
