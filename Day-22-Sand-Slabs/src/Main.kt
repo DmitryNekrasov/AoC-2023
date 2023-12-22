4361bcdd757551d2d4e6cccd5b20ad17e46893e1
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-22 16:13:42 +0400

    Day 22: Parse data

--- Day-22-Sand-Slabs/src/Main.kt
+++ Day-22-Sand-Slabs/src/Main.kt
@@ -1,3 +1,31 @@
+import java.io.File
+
+data class Point(val x: Int, val y: Int, val z: Int)
+
+data class Brick(val p1: Point, val p2: Point)
+
+fun parseBrick(brickString: String): Brick {
+    fun parsePoint(pointString: String): Point =
+        pointString.split(",").map { it.toInt() }.let { (x, y, z) -> Point(x, y, z) }
+
+    fun Boolean.toInt() = if (this) 1 else 0
+
+    return brickString.split("~")
+        .let { (p1String, p2String) ->
+            Brick(parsePoint(p1String), parsePoint(p2String))
+                .also { brick -> if (brick.p1.z > brick.p2.z) throw RuntimeException() }
+                .also { brick ->
+                    if ((brick.p1.x == brick.p2.x).toInt() +
+                        (brick.p1.y == brick.p2.y).toInt() +
+                        (brick.p1.z == brick.p2.z).toInt() < 2
+                    ) throw RuntimeException().also { println(brick) }
+                }
+        }
+}
+
 fun main() {
-    println("Hello!")
+    val bricks = File("input_simple_1.txt").useLines { it.toList() }.map { parseBrick(it) }
+
+    bricks.joinToString("\n") { it.toString() }
+        .also { println(it) }
 }
