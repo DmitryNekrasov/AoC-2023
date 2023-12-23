e0d23e93b7b9d9d651be0376c213247e9774b8a8
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-23 18:48:54 +0400

    Day 23: crossroads

--- Day-23-A-Long-Walk/src/Main.kt
+++ Day-23-A-Long-Walk/src/Main.kt
@@ -1,65 +1,40 @@
 import java.io.File
-import kotlin.math.max
 
-fun crossroads(maze: List<String>): Pair<Int, Int> {
+data class Point(val i: Int, val j: Int)
+
+fun Char.isFree() = this != '#'
+fun Boolean.toInt() = if (this) 1 else 0
+
+fun crossroads(maze: List<String>): List<Point> {
     val n = maze.size
     val m = maze.first().length
-    var direct = 0
-    var reverse = 0
+    val result = mutableListOf(Point(0, 1), Point(n - 1, m - 2))
     for (i in 1..<n - 1) {
         for (j in 1..<m - 1) {
             if (maze[i][j] == '.') {
-                if (maze[i][j + 1] == '>' && maze[i + 1][j] == 'v') direct++
-                if (maze[i][j - 1] == '>' && maze[i - 1][j] == 'v') reverse++
+                if (listOf(maze[i - 1][j], maze[i + 1][j], maze[i][j - 1], maze[i][j + 1])
+                        .sumOf { it.isFree().toInt() } >= 3
+                ) {
+                    result.add(Point(i, j))
+                }
             }
         }
     }
-    return direct to reverse
+    return result
 }
 
+fun compress(maze: List<String>) {
+    val crossroads = crossroads(maze)
+    println("crossroads number = ${crossroads.size}")
+    println(crossroads)
+}
 
-fun solvePartOne(mazeStrings: List<String>): Int {
-    data class Point(val i: Int, val j: Int)
-
-    val finish = Point(mazeStrings.lastIndex, mazeStrings.first().lastIndex - 1)
-
-    fun backtrack(prevIn: Point, currentIn: Point, mazeIn: List<CharArray>): Int {
-        var prev = prevIn
-        var current = currentIn
-        val maze = mazeIn.map { it.clone() }
-
-        fun Point.next(): List<Point> {
-            return listOf(Point(i + 1, j), Point(i - 1, j), Point(i, j + 1), Point(i, j - 1))
-                .filter { it != prev }
-                .filter { (i, j) ->
-                    maze[i][j] == '.' ||
-                            current.j < j && maze[i][j] == '>' ||
-                            current.i < i && maze[i][j] == 'v'
-                }
-        }
-
-        var distance = 1
-        while (current != finish) {
-            maze[current.i][current.j] = '#'
-            val next = current.next()
-            if (next.isEmpty()) return 0
-            if (next.size > 1) return distance + max(
-                backtrack(current, next.first(), maze),
-                backtrack(current, next.last(), maze)
-            )
-            distance++
-            prev = current
-            current = next.first()
-        }
-        return distance
-    }
-
-    return backtrack(Point(0, 1), Point(1, 1), mazeStrings.map { it.toCharArray() })
+fun solvePartOne(maze: List<String>): Int {
+    compress(maze)
+    return -1
 }
 
 fun main() {
     val maze = File("input.txt").useLines { it.toList() }
-    val (direct, reverse) = crossroads(maze)
-    println("direct = $direct, reverse = $reverse")
     println(solvePartOne(maze))
 }
