562a15059b6466fabc0c4e3c41c2a467d30ca2b0
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-23 15:22:02 +0400

    Day 23: Part 1

--- Day-23-A-Long-Walk/src/Main.kt
+++ Day-23-A-Long-Walk/src/Main.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import kotlin.math.max
 
 fun crossroads(maze: List<String>): Pair<Int, Int> {
     val n = maze.size
@@ -16,10 +17,48 @@ fun crossroads(maze: List<String>): Pair<Int, Int> {
     return direct to reverse
 }
 
+
+fun solve(mazeStrings: List<String>): Int {
+    data class Point(val i: Int, val j: Int)
+
+    val finish = Point(mazeStrings.lastIndex, mazeStrings.first().lastIndex - 1)
+
+    fun backtrack(prevIn: Point, currentIn: Point, mazeIn: List<CharArray>): Int {
+        var prev = prevIn
+        var current = currentIn
+        val maze = mazeIn.map { it.clone() }
+
+        fun Point.next(): List<Point> {
+            return listOf(Point(i + 1, j), Point(i - 1, j), Point(i, j + 1), Point(i, j - 1))
+                .filter { it != prev }
+                .filter { (i, j) ->
+                    maze[i][j] == '.' ||
+                            current.j < j && maze[i][j] == '>' ||
+                            current.i < i && maze[i][j] == 'v'
+                }
+        }
+
+        var distance = 1
+        while (current != finish) {
+            val next = current.next()
+            if (next.isEmpty()) return 0
+            if (next.size == 2) return distance + max(
+                backtrack(current, next.first(), maze),
+                backtrack(current, next.last(), maze)
+            )
+            distance++
+            prev = current
+            current = next.first()
+        }
+        return distance
+    }
+
+    return backtrack(Point(0, 1), Point(1, 1), mazeStrings.map { it.toCharArray() })
+}
+
 fun main() {
-    val maze = File("input_simple_1.txt").useLines { it.toList() }
+    val maze = File("input.txt").useLines { it.toList() }
     val (direct, reverse) = crossroads(maze)
     println("direct = $direct, reverse = $reverse")
-    maze.joinToString("\n")
-        .also { println(it) }
+    println(solve(maze))
 }
