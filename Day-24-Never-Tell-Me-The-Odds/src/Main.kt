18a4d0e4a71e062e95ace5367a46b2a9125084da
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-24 14:13:10 +0400

    Day 24: Add class Line

--- Day-24-Never-Tell-Me-The-Odds/src/Main.kt
+++ Day-24-Never-Tell-Me-The-Odds/src/Main.kt
@@ -1,11 +1,25 @@
 import java.io.File
+import java.math.BigInteger
 
-data class Point(val x: Long, val y: Long, val z: Long)
+data class Point(val x: Long, val y: Long, val z: Long) {
+    operator fun plus(other: Point) = Point(x + other.x, y + other.y, z + other.z)
+}
 
-fun parseLine(line: String): Pair<Point, Point> {
+data class Line(val p: Point, val q: Point) {
+    val a = p.y - q.y
+    val b = q.x - p.x
+    val c = -a * p.x - b * p.y
+
+    override fun toString(): String {
+        return "a = $a, b = $b, c = $c"
+    }
+}
+
+fun parseLine(line: String): Line {
     fun parsePoint(pointString: String) =
         pointString.split(", ").map { it.toLong() }.let { (x, y, z) -> Point(x, y, z) }
-    return line.split(" @ ").map { parsePoint(it) }.let { (p1, p2) -> p1 to p2 }
+    val (p1, velocity) = line.split(" @ ").map { parsePoint(it) }
+    return Line(p1, p1 + velocity)
 }
 
 fun main() {
