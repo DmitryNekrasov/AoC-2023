c2509e4c583cc8ef37f67dc25604a46c9c2b8ef5
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-24 14:27:58 +0400

    Day 24: Add parallel, equivalent

--- Day-24-Never-Tell-Me-The-Odds/src/Main.kt
+++ Day-24-Never-Tell-Me-The-Odds/src/Main.kt
@@ -1,24 +1,35 @@
 import java.io.File
 import java.math.BigDecimal
-import java.math.BigInteger
 
-data class Point(val x: Long, val y: Long, val z: Long) {
+data class Point(val x: BigDecimal, val y: BigDecimal, val z: BigDecimal) {
     operator fun plus(other: Point) = Point(x + other.x, y + other.y, z + other.z)
 }
 
+fun det(a: BigDecimal, b: BigDecimal, c: BigDecimal, d: BigDecimal) = a * d - b * c
+
 class Line(p: Point, q: Point) {
-    val a = BigDecimal.valueOf(p.y) - BigDecimal.valueOf(q.y)
-    val b = BigDecimal.valueOf(q.x) - BigDecimal.valueOf(p.x)
-    val c = -a * BigDecimal.valueOf(p.x) - b * BigDecimal.valueOf(p.y)
+    val a = p.y - q.y
+    val b = q.x - p.x
+    val c = -a * p.x - b * p.y
+
+    infix fun parallel(other: Line) = det(a, b, other.a, other.b).abs() < EPS
+
+    infix fun equivalent(other: Line) = det(a, b, other.a, other.b).abs() < EPS &&
+            det(a, c, other.a, other.c).abs() < EPS &&
+            det(b, c, other.b, other.c).abs() < EPS
 
     override fun toString(): String {
         return "a = $a, b = $b, c = $c"
+    }
+
+    companion object {
+        val EPS = BigDecimal("0.00000000001")
     }
 }
 
 fun parseLine(line: String): Line {
     fun parsePoint(pointString: String) =
-        pointString.split(", ").map { it.toLong() }.let { (x, y, z) -> Point(x, y, z) }
+        pointString.split(", ").map { BigDecimal.valueOf(it.toLong()) }.let { (x, y, z) -> Point(x, y, z) }
     val (p1, velocity) = line.split(" @ ").map { parsePoint(it) }
     return Line(p1, p1 + velocity)
 }
