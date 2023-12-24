86b5850a884706435973964a2ffd0d62c7545deb
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-24 16:02:18 +0400

    Day 24: Part 1

--- Day-24-Never-Tell-Me-The-Odds/src/Main.kt
+++ Day-24-Never-Tell-Me-The-Odds/src/Main.kt
@@ -1,8 +1,9 @@
 import java.io.File
 import java.math.BigDecimal
+import java.math.MathContext
 
-val BOTTOM_LIMIT = BigDecimal("7")
-val UPPER_LIMIT = BigDecimal("27")
+val BOTTOM_LIMIT = BigDecimal("200000000000000")
+val UPPER_LIMIT = BigDecimal("400000000000000")
 
 data class Point(val x: BigDecimal, val y: BigDecimal, val z: BigDecimal) {
     operator fun plus(other: Point) = Point(x + other.x, y + other.y, z + other.z)
@@ -10,31 +11,32 @@ data class Point(val x: BigDecimal, val y: BigDecimal, val z: BigDecimal) {
     fun inLimit() = x in BOTTOM_LIMIT..UPPER_LIMIT && y in BOTTOM_LIMIT..UPPER_LIMIT
 }
 
+fun distance(p: Point, q: Point): BigDecimal =
+    ((p.x - q.x) * (p.x - q.x) + (p.y - q.y) * (p.y - q.y)).sqrt(MathContext(10))
+
 fun det(a: BigDecimal, b: BigDecimal, c: BigDecimal, d: BigDecimal) = a * d - b * c
 
 data class Direction(val x: Int, val y: Int)
 
 fun direction(p: Point, q: Point) = Direction((p.x - q.x).signum(), (p.y - q.y).signum())
 
-class Line(private val p: Point, velocity: Point) {
+class Line(private val p: Point, velocityPoint: Point) {
     private val a: BigDecimal
     private val b: BigDecimal
     private val c: BigDecimal
     private val direction: Direction
+    private val velocity: BigDecimal
 
     init {
-        val q = p + velocity
+        val q = p + velocityPoint
         a = p.y - q.y
         b = q.x - p.x
         c = -a * p.x - b * p.y
         direction = direction(p, q)
+        velocity = distance(p, velocityPoint)
     }
 
-    private infix fun parallel(other: Line) = (det(a, b, other.a, other.b).abs() < EPS).also { if (it) println("PARALLEL") }
-
-    private infix fun equivalent(other: Line) = (det(a, b, other.a, other.b).abs() < EPS &&
-            det(a, c, other.a, other.c).abs() < EPS &&
-            det(b, c, other.b, other.c).abs() < EPS).also { if (it) println("EQUIVALENT") }
+    private infix fun parallel(other: Line) = det(a, b, other.a, other.b).abs() < EPS
 
     private fun intersect(other: Line): Point {
         val zn = det(a, b, other.a, other.b)
@@ -44,12 +46,8 @@ class Line(private val p: Point, velocity: Point) {
     infix fun collide(other: Line): Boolean {
         if (this parallel other) return false
         val intersection = intersect(other)
-        if (intersection.inLimit() &&
-            direction == direction(p, intersection) && other.direction == direction(other.p, intersection)
-        ) {
-            return true
-        }
-        return false
+        return intersection.inLimit() &&
+                direction == direction(p, intersection) && other.direction == direction(other.p, intersection)
     }
 
     override fun toString(): String {
@@ -57,23 +55,17 @@ class Line(private val p: Point, velocity: Point) {
     }
 
     companion object {
-        val EPS = BigDecimal("0.00000000001")
+        val EPS = BigDecimal("0.000000000001")
     }
 }
 
 fun solve(lines: List<Line>): Int {
-    lines.joinToString("\n") { it.toString() }
-        .also { println(it) }
-    println("Line number = ${lines.size}")
     var result = 0
     for (i in lines.indices) {
         val line1 = lines[i]
         for (j in i + 1..lines.lastIndex) {
             val line2 = lines[j]
             if (line1 collide line2) {
-                println(line1)
-                println(line2)
-                println("--------------------------------------------------------------------------------")
                 result++
             }
         }
@@ -88,6 +80,6 @@ fun parseLine(line: String): Line {
 }
 
 fun main() {
-    val lines = File("input_simple_1.txt").useLines { it.toList() }.map { parseLine(it) }
+    val lines = File("input.txt").useLines { it.toList() }.map { parseLine(it) }
     println(solve(lines))
 }
