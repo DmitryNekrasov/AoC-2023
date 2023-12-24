69b7e4ed4d47ab1e6c7ad104c6260933c8b49c76
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-24 15:22:13 +0400

    Day 24: Line intersection

--- Day-24-Never-Tell-Me-The-Odds/src/Main.kt
+++ Day-24-Never-Tell-Me-The-Odds/src/Main.kt
@@ -1,25 +1,35 @@
 import java.io.File
 import java.math.BigDecimal
 
+val BOTTOM_LIMIT = BigDecimal("7")
+val UPPER_LIMIT = BigDecimal("27")
+
 data class Point(val x: BigDecimal, val y: BigDecimal, val z: BigDecimal) {
     operator fun plus(other: Point) = Point(x + other.x, y + other.y, z + other.z)
+
+    fun inLimit() = x in BOTTOM_LIMIT..UPPER_LIMIT && y in BOTTOM_LIMIT..UPPER_LIMIT
 }
 
 fun det(a: BigDecimal, b: BigDecimal, c: BigDecimal, d: BigDecimal) = a * d - b * c
 
-class Line(p: Point, q: Point) {
+class Line(val p: Point, val q: Point) {
     val a = p.y - q.y
     val b = q.x - p.x
     val c = -a * p.x - b * p.y
 
-    infix fun parallel(other: Line) = det(a, b, other.a, other.b).abs() < EPS
+    infix fun parallel(other: Line) = (det(a, b, other.a, other.b).abs() < EPS).also { if (it) println("PARALLEL") }
 
-    infix fun equivalent(other: Line) = det(a, b, other.a, other.b).abs() < EPS &&
+    infix fun equivalent(other: Line) = (det(a, b, other.a, other.b).abs() < EPS &&
             det(a, c, other.a, other.c).abs() < EPS &&
-            det(b, c, other.b, other.c).abs() < EPS
+            det(b, c, other.b, other.c).abs() < EPS).also { if (it) println("EQUIVALENT") }
+
+    infix fun intersect(other: Line): Point {
+        val zn = det(a, b, other.a, other.b)
+        return Point(-det(c, b, other.c, other.b) / zn, -det(a, c, other.a, other.c) / zn, BigDecimal.ZERO)
+    }
 
     override fun toString(): String {
-        return "a = $a, b = $b, c = $c"
+        return "p = $p, q = $q, a = $a, b = $b, c = $c"
     }
 
     companion object {
@@ -27,15 +37,34 @@ class Line(p: Point, q: Point) {
     }
 }
 
+fun solve(lines: List<Line>): Int {
+    lines.joinToString("\n") { it.toString() }
+        .also { println(it) }
+    println("Line number = ${lines.size}")
+    var result = 0
+    for (i in lines.indices) {
+        val line1 = lines[i]
+        for (j in i + 1..lines.lastIndex) {
+            val line2 = lines[j]
+            if (!(line1 parallel line2) && (line1 equivalent line2 || (line1 intersect line2).inLimit())) {
+                println(line1)
+                println(line2)
+                println("--------------------------------------------------------------------------------")
+                result++
+            }
+        }
+    }
+    return result
+}
+
 fun parseLine(line: String): Line {
     fun parsePoint(pointString: String) =
-        pointString.split(", ").map { BigDecimal.valueOf(it.toLong()) }.let { (x, y, z) -> Point(x, y, z) }
+        pointString.split(", ").map { BigDecimal.valueOf(it.trim().toLong()) }.let { (x, y, z) -> Point(x, y, z) }
     val (p1, velocity) = line.split(" @ ").map { parsePoint(it) }
     return Line(p1, p1 + velocity)
 }
 
 fun main() {
-    val points = File("input.txt").useLines { it.toList() }.map { parseLine(it) }
-    points.joinToString("\n") { it.toString() }
-        .also { println(it) }
+    val lines = File("input_simple_1.txt").useLines { it.toList() }.map { parseLine(it) }
+    println(solve(lines))
 }
