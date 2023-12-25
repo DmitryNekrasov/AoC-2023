569219e872bb44168c607b892f6024b955cd735c
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-25 13:35:22 +0400

    Day 25: Add empty solve method

--- Day-25-Snowverload/src/Main.kt
+++ Day-25-Snowverload/src/Main.kt
@@ -1,14 +1,32 @@
 import java.io.File
 
-fun parseLine(line: String): Pair<String, Set<String>> {
+typealias Edges = List<Pair<String, String>>
+typealias Graph = HashMap<String, MutableList<String>>
+
+fun parseLine(line: String): Edges {
     val (from, toList) = line.split(": ")
-    val to = toList.split(" ").toSet()
-    return from to to
+    return toList.split(" ").map { from to it }
+}
+
+fun Edges.toGraph(): Graph {
+    val result = Graph()
+    for ((from, to) in this) {
+        result[from] = (result[from] ?: mutableListOf()).also { it.add(to) }
+        result[to] = (result[to] ?: mutableListOf()).also { it.add(from) }
+    }
+    return result
+}
+
+fun solve(edges: Edges, graph: Graph): Int {
+    println(edges)
+    graph.toList().joinToString("\n") { it.toString() }
+        .also { println(it) }
+
+    return -1
 }
 
 fun main() {
-    val graph = File("input_simple_1.txt").useLines { it.toList() }.map { parseLine(it) }
-        .associateBy({ it.first }, { it.second })
-    graph.toList().joinToString("\n") { it.toString() }
-        .also { println(it) }
+    val edges = File("input_simple_1.txt").useLines { it.toList() }.map { parseLine(it) }.flatten()
+    val graph = edges.toGraph()
+    println(solve(edges, graph))
 }
