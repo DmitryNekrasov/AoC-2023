9c203f31990589407fc70e9f0e5ade5e58345d6a
Dmitry Nekrasov <scannorone@gmail.com>
2023-12-25 13:03:44 +0400

    Day 25: Parse data

--- Day-25-Snowverload/src/Main.kt
+++ Day-25-Snowverload/src/Main.kt
@@ -1,3 +1,14 @@
+import java.io.File
+
+fun parseLine(line: String): Pair<String, Set<String>> {
+    val (from, toList) = line.split(": ")
+    val to = toList.split(" ").toSet()
+    return from to to
+}
+
 fun main() {
-    println("Hello World!")
+    val graph = File("input_simple_1.txt").useLines { it.toList() }.map { parseLine(it) }
+        .associateBy({ it.first }, { it.second })
+    graph.toList().joinToString("\n") { it.toString() }
+        .also { println(it) }
 }
